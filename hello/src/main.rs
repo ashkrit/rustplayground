f03cb37db145295f5ec5cd2323dc8d6037989625
Ashkrit <ashkrit@gmail.com>
2022-02-13 13:04:48 +0800

    Some readme notes

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Lets play guessing game!!!");
+    println!("Input your guess.");
+
+    let mut guess = String::new(); // Allocates new String
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guess it {}", guess);
 }
