62bc7b62b2c8fb24280785ec589e0ed58d3b6865
ashkrit@gmail.com <Crack@it12>
2023-09-16 21:23:06 +0800

    Add notes

---
+++ database/db-client/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let tree = sled::open("/Users/ashkrit/_tmp/welcome-to-sled")
+        .expect("open");
+
+    //generate value from 1 to 10
+
+    let no_of_items = 100;
+    for i in 1..no_of_items {
+        let v = format!("VAL{}", i);
+        let k = format!("{}{}", "KEY", i);
+        let _ = tree.insert(k, v.as_str());
+    }
+    assert_eq!(tree.get(&"KEY1"), Ok(Some(sled::IVec::from("VAL1"))));
+
+
+    println!("Capacity :{}", tree.len());
+
+    let range = "KEY1".."KEY9";
+    println!("Key Count {}", tree.range(range).count());
+
+    let result = tree.get("KEY1");
+    println!("{}", as_string(result.unwrap().unwrap()));
+
+}
+
+fn as_string(x: sled::IVec) -> String {
+    String::from_utf8(x.to_vec()).unwrap()
+}
