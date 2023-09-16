a7b945fdda79ec6f8780b50b1ca3cc6718a884b0
ashkrit@gmail.com <Crack@it12>
2023-09-17 00:30:06 +0800

    Struct with functions

--- rust_day04_datatypes/src/main.rs
+++ rust_day04_datatypes/src/main.rs
@@ -1,9 +1,12 @@
 
 
 mod struct_basic;
+mod struct_fns;
 
 
-fn main() {
+ fn main() {
 
-    struct_basic::main()
+    //struct_basic::main()
+
+    struct_fns::main()
 }
---
+++ rust_day04_datatypes/src/struct_fns.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+
+}
+
+pub fn main() {
+    let f = File::new("f.txt");
+    println!("{:?}", f)
+}
