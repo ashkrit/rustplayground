1e43fea00a03d51e66434e3a6b7554ede03bf820
ashkrit@gmail.com <Crack@it12>
2023-09-16 23:05:01 +0800

    Vector example

---
+++ rust_day03/src/vectors.rs
@@ -0,0 +1,47 @@
+pub fn main() {
+    let haystack = "\
+ Every face, every shop,
+  bedroom window, public-house, and
+  dark square is a picture
+  feverishly turned--in search of what?
+  It is the same with books.
+ What do we seek
+ through millions of pages?";
+
+    let ctx_lines = 2;
+    let needle = "oo";
+
+    let mut tags: Vec<usize> = vec![];
+    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+
+    for (i, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(i);
+        }
+
+        let v = Vec::with_capacity(2 * ctx_lines + 1);
+        ctx.push(v);
+    }
+
+    if tags.is_empty() {
+        return;
+    }
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(ctx_lines);
+            let upper_bound = tag + ctx_lines;
+            if (i >= lower_bound) && (i <= upper_bound) {
+                let line_as_string = String::from(line);
+                let local_ctx = (i, line_as_string);
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+
+    for local_ctx in ctx.iter() {
+        for (index, v) in local_ctx.iter() {
+            println!("{} -> {:?}", index, v)
+        }
+    }
+}
