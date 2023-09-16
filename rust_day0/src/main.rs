c588e70856c43b9e6cd2267a91d64210b9ad4f51
ashkrit@gmail.com <Crack@it12>
2023-09-16 13:03:40 +0800

    Some basic examples

--- rust_day0/src/main.rs
+++ rust_day0/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[derive(Debug)]
+enum Cereal{
+    Barley, Millet, Rice,
+    Rye, Spelt, Wheat,
 }
+
+fn main() {
+
+    let mut grains:Vec<Cereal> = vec![];
+
+    grains.push(Cereal::Barley);
+    grains.push(Cereal::Millet);
+
+    println!("{:?}", grains);
+
+    //drop(grains); // this line will cause error because it is deallocating the memory
+    println!("{:?}", grains);
+
+}
+
+
+
+
