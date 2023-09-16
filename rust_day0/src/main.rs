ed09c05b24eb5b17973c7f0f7d99e636bab3f85c
ashkrit@gmail.com <Crack@it12>
2023-09-16 13:09:44 +0800

    Refactor

--- rust_day0/src/main.rs
+++ rust_day0/src/main.rs
@@ -8,7 +8,12 @@ enum Cereal{
 
 fn main() {
 
-    let mut grains:Vec<Cereal> = vec![];
+    simple_dereference();
+
+}
+
+fn simple_dereference() {
+    let mut grains: Vec<Cereal> = vec![];
 
     grains.push(Cereal::Barley);
     grains.push(Cereal::Millet);
@@ -17,8 +22,12 @@ fn main() {
 
     //drop(grains); // this line will cause error because it is deallocating the memory
     println!("{:?}", grains);
-
 }
+
+fn threads() {
+    let data =100;
+}
+
 
 
 
