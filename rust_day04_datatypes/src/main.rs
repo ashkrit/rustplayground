4836672c8b9dd5a58392270b7aab910f7b025fd8
ashkrit@gmail.com <Crack@it12>
2023-09-17 00:23:25 +0800

    Simple struct examples

--- rust_day04_datatypes/src/main.rs
+++ rust_day04_datatypes/src/main.rs
@@ -1,47 +1,9 @@
-#[derive(Debug)]
-struct File {
-    name: String,
-    date: Vec<u8>,
-}
 
-struct Hostname(String);
 
-fn connect(host: Hostname) {
-    println!("connected to {}", host.0);
-}
+mod struct_basic;
 
-fn open(f: &mut File) -> bool {
-    true
-}
-
-fn close(f: &mut File) -> bool {
-    true
-}
-
-fn read(f: &File, buffer: &mut Vec<u8>) -> usize {
-    let mut tmp = f.date.clone();
-    let data_length = tmp.len();
-    buffer.append(&mut tmp);
-
-    return data_length;
-}
 
 fn main() {
-    let mut f = File {
-        name: String::from("readme.md"),
-        date: vec![100, 200, 191, 232],
-    };
 
-    println!("{:?}", f);
-    println!("{} has {} bytes", f.name, f.date.len());
-
-    let h = Hostname(String::from("localhost"));
-    connect(h);
-
-    let mut buffer = vec![];
-
-    open(&mut f);
-    let bytes = read(&f, &mut buffer);
-    close(&mut f);
-    println!("{} bytes read from {:?}", bytes, buffer);
+    struct_basic::main()
 }
---
+++ rust_day04_datatypes/src/struct_basic.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct File {
+    pub(crate) name: String,
+    pub(crate) date: Vec<u8>,
+}
+
+struct Hostname(String);
+
+fn connect(host: Hostname) {
+    println!("connected to {}", host.0);
+}
+
+ fn open(f: &mut File) -> bool {
+    true
+}
+
+ fn close(f: &mut File) -> bool {
+    true
+}
+
+ fn read(f: &File, buffer: &mut Vec<u8>) -> usize {
+    let mut tmp = f.date.clone();
+    let data_length = tmp.len();
+    buffer.append(&mut tmp);
+
+    return data_length;
+}
+
+pub fn main() {
+    let mut f = File {
+        name: String::from("readme.md"),
+        date: vec![100, 200, 191, 232],
+    };
+
+    println!("{:?}", f);
+    println!("{} has {} bytes", f.name, f.date.len());
+
+    let h = Hostname(String::from("localhost"));
+    connect(h);
+
+    let mut buffer = vec![];
+
+    open(&mut f);
+    let bytes = read(&f, &mut buffer);
+    close(&mut f);
+    println!("{} bytes read from {:?}", bytes, buffer);
+}
