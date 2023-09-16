583d87b80fa03df35fb64ee453b1baa5aecfe366
ashkrit@gmail.com <Crack@it12>
2023-09-16 13:19:58 +0800

    Add notes

--- rust_day0/src/main.rs
+++ rust_day0/src/main.rs
@@ -6,12 +6,19 @@ enum Cereal{
     Rye, Spelt, Wheat,
 }
 
+/**
+This program demonstrate how Rust provide safety with concurrent access , dereference and array access
+none of this will even compile !. This is beauty of rust. Dead code is also not complied if it has got concurrency issue.
+**/
+
 fn main() {
 
     //simple_dereference();
     //threads();
+    //array_access();
 }
 
+/*
 fn simple_dereference() {
     let mut grains: Vec<Cereal> = vec![];
 
@@ -20,13 +27,14 @@ fn simple_dereference() {
 
     println!("{:?}", grains);
 
-    //drop(grains); // this line will cause error because it is deallocating the memory
+    drop(grains); // this line will cause error because it is deallocating the memory
     println!("{:?}", grains);
 }
 
 fn threads() {
     let mut data =100;
 
+    //this block of code cause issue with race condition
     thread::spawn(|| {data =200});
     thread::spawn(|| {data =300});
 
@@ -34,6 +42,15 @@ fn threads() {
 
 }
 
+
+
+fn array_access() {
+    let fruits = ["Apple", "Banana", "Orange"];
+    let over_flow_item = fruits[4]; // this line will cause error because it is out of bound
+}
+
+
+ */
 
 
 
